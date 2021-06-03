1a476efff25827bfe556ef3d80479f99ddaff4c7
benchittle <72046603+benchittle@users.noreply.github.com>
2021-06-03 00:14:21 -0400

    Update LCP extraction functions for new approach

--- beach_dune_formatter/extraction_tools.py
+++ beach_dune_formatter/extraction_tools.py
@@ -223,17 +223,17 @@ def _identify_toe_lcp_old(profile_xy):
 
 def identify_toe_lcp(xy_data, shore_x, crest_x, columns): 
     # Insert columns for the shore and crest values for each profile in xy_data.
-    xy_data["shore_x"] = shore_x.set_index(["state", "segment", "profile"])["x"].reindex_like(xy_data)
-    xy_data["crest_x"] = crest_x.set_index(["state", "segment", "profile"])["x"].reindex_like(xy_data)
+    xy_data["shore_x"] = shore_x
+    xy_data["crest_x"] = crest_x
 
     # Apply the old LCP toe identification function profile-wise on the 
     # DataFrame.
-    toes = xy_data.dropna().groupby(["state", "segment", "profile"]).apply(_identify_toe_lcp_old).rename("x")
+    toe = xy_data.dropna().groupby(xy_data.index.names).apply(_identify_toe_lcp_old).rename("x")
     # Extract the rows corresponding to each identified toe in the original data.
-    toes = xy_data.set_index("x", append=True).loc[pd.MultiIndex.from_frame(toes.reset_index())]
+    toe = xy_data.set_index("x", append=True).loc[pd.MultiIndex.from_frame(toe.reset_index())]
 
     # Return the data for the selected columns.
-    return toes.reset_index()[columns]
+    return toe.reset_index("x")[columns]
 
 
 ############################### CREST FUNCTIONS ###############################
